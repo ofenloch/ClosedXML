777652da401992c938528673a92c6fb1784833c3
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2022-08-29 13:53:35 +0200

    add test method Iteration

--- ClosedXML.Tests/Excel/CalcEngine/CalcEngineExceptionTests.cs
+++ ClosedXML.Tests/Excel/CalcEngine/CalcEngineExceptionTests.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using ClosedXML.Excel.CalcEngine.Exceptions;
+using ClosedXML.Tests.Utils;
 using NUnit.Framework;
 using System;
 using System.Globalization;
@@ -52,5 +53,79 @@ namespace ClosedXML.Tests.Excel.CalcEngine
 
             Assert.AreEqual("Success", ws.Cell("A2").Value);
         }
+
+        [Test]
+        [TestCase(true, (uint)1, 0.001)]
+        [TestCase(true, (uint)2, 0.001)]
+        [TestCase(true, (uint)3, 0.001)]
+        [TestCase(true, (uint)50, 0.01)]
+        [TestCase(false, (uint)50, 0.01)]
+        public void Iteration(bool iterate, uint iterateCount, double iterateDelta)
+        {
+            using (var tmpFile = new TemporaryFile())
+            {
+                var saveOptions = new SaveOptions { EvaluateFormulasBeforeSaving = true };
+
+                using (var wb = new XLWorkbook())
+                {
+                    wb.Iterate = iterate;
+                    wb.IterateCount = iterateCount;
+                    wb.IterateDelta = iterateDelta;
+
+                    var ws = wb.AddWorksheet("Iteration");
+
+                    ws.Cell("A1").Value = "Fibonacci";
+
+                    var b2 = ws.Cell("B2");
+                    var b3 = ws.Cell("B3");
+                    var b4 = ws.Cell("B4");
+
+                    b2.FormulaA1 = "=IF( ISNUMBER(B3), B3, 1 )";
+                    var dummy_b2 = b2.Value;
+                    b3.FormulaA1 = "=IF( ISNUMBER(B4), B4, 1 )";
+                    var dummy_b3 = b3.Value;
+                    b4.FormulaA1 = "=IF( AND( ISNUMBER(B3), ISNUMBER(B2)), B2+B3, 1 )";
+
+                    wb.SaveAs(tmpFile.Path, saveOptions);
+                }
+
+                using (var wb = new XLWorkbook(tmpFile.Path))
+                {
+                    Assert.AreEqual(wb.Iterate, iterate);
+                    if (wb.Iterate == true)
+                    {
+                        Assert.AreEqual(wb.IterateCount, iterateCount);
+                        Assert.AreEqual(wb.IterateDelta, iterateDelta);
+                    }
+                    else
+                    {
+                        Assert.AreEqual(wb.IterateCount, 0);
+                        Assert.AreEqual(wb.IterateDelta, 0);
+                    }
+                    var ws = wb.Worksheet(1);
+
+                    if (wb.Iterate == true)
+                    {
+                        ws.RecalculateAllFormulas();
+                        var val_b2 = ws.Cell("B2").Value;
+                        var val_b3 = ws.Cell("B3").Value;
+                        var val_b4 = ws.Cell("B4").Value;
+                        Assert.AreEqual(val_b2, 2);
+                        Assert.AreEqual(val_b3, 2);
+                        Assert.AreEqual(val_b4, 4);
+                    }
+                    else
+                    {
+                        var getValueB2 = new TestDelegate(() => { var v = ws.Cell("B2").Value; });
+                        var getValueB3 = new TestDelegate(() => { var v = ws.Cell("B3").Value; });
+                        var getValueB4 = new TestDelegate(() => { var v = ws.Cell("B4").Value; });
+                        Exception ex;
+                        ex = Assert.Throws<System.InvalidOperationException>( getValueB2 );
+                        ex = Assert.Throws<System.InvalidOperationException>( getValueB3 );
+                        ex = Assert.Throws<System.InvalidOperationException>( getValueB4 );
+                    }
+                }
+            } // using (var tmpFile = new TemporaryFile())
+        } // public void Iteration(bool iterate, uint iterateCount, double iterateDelta)
     }
 }
